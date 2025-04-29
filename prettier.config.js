5141f6779d11ac68e24415708b95bd2179521a8e
Emithen <86219540+Emithen@users.noreply.github.com>
2025-04-29 17:19:44 +0900

    Initial Commit

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+// prettier.config.js
+export default {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 100,
+  tabWidth: 2,
+  quoteProps: 'preserve',
+  plugins: ['prettier-plugin-tailwindcss'],
+};
