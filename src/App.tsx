a956540a33697c89bd0d1ffdcee95f2c63433bce
jinj00oo <icems0428@naver.com>
2025-05-04 00:18:40 +0900

    로그인 페이지 작업 완료 (임시)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+<<<<<<< HEAD
+=======
+import LoginPage from './pages/LoginPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+>>>>>>> 98745ab (로그인 페이지 작업 완료 (임시))
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,69 @@
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const schema = z.object({
+  id: z
+    .string()
+    .min(8, { message: '아이디는 8자 이상이어야 합니다.' })
+    .max(20, { message: '아이디는 20자 이하여야 합니다.' }),
+  password: z
+    .string()
+    .min(8, { message: '비밀번호는 8자 이상이어야 합니다.' })
+    .max(20, { message: '비밀번호는 20자 이하여야 합니다.' }),
+});
+
+type LoginFields = z.infer<typeof schema>;
+
+const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFields>({
+    defaultValues: {
+      id: '',
+      password: '',
+    },
+    resolver: zodResolver(schema),
+    mode: 'onBlur',
+  });
+
+  const onSubmit = async (data: LoginFields) => {
+    await console.log(data);
+  };
+
+  return (
+    <div className="flex h-dvh flex-col items-center justify-center gap-5">
+      <h1 className="mb-10 flex text-center text-6xl">
+        SERI
+        <br />
+        VOCA
+      </h1>
+      <div className="flex flex-col gap-3">
+        <input
+          {...register('id')}
+          className="h-10 w-60 rounded-md bg-gray-200 p-2 placeholder-gray-600"
+          placeholder="아이디를 입력하세요..."
+        ></input>
+        {errors.id && <div className="text-sm text-red-500">{errors.id.message}</div>}
+        <input
+          {...register('password')}
+          type="password"
+          className="h-10 w-60 rounded-md bg-gray-200 p-2 placeholder-gray-600"
+          placeholder="비밀번호를 입력하세요..."
+        ></input>
+        {errors.password && <div className="text-sm text-red-500">{errors.password.message}</div>}
+      </div>
+      <button
+        onClick={handleSubmit(onSubmit)}
+        className="h-10 w-20 cursor-pointer rounded-md bg-yellow-400 p-2"
+      >
+        로그인
+      </button>
+      <button className="mt-15 cursor-pointer underline">회원가입</button>
+    </div>
+  );
+};
+
+export default LoginPage;
