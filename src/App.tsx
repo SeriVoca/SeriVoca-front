9d460cb4495abb096b9867ad29565e59af6cb964
jinj00oo <icems0428@naver.com>
2025-06-02 19:00:39 +0900

    design: 화면 크기 설정방식 변경 및 프리티어 전체 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,15 @@ import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './layout/Layout';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <Layout>
-                <HomePage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <Layout>
-                <LoginPage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/signup"
-            element={
-              <Layout>
-                <SignupPage />
-              </Layout>
-            }
-          />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
           /** Protected Route 혹은 Private Route 구현 필요 */
         </Routes>
       </BrowserRouter>
